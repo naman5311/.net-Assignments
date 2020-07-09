14a501fdb62ff5ad8155ea77666361ae01611fcf
Naman <naman.5311@gmail.com>
2020-07-10 00:46:39 +0530

    Assignment 1

---
+++ HandsOnAssignment/HandsOnAssignment/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HandsOnAssignment.Models;
+
+namespace HandsOnAssignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> Plist = new List<Product>()
+        {
+            new Product() { Pid="P001",Pname="Chair",Price=600,Stock=200 }
+        };
+        [Route("GetAllData")]
+        public IActionResult Index()
+        {
+            return View(Plist);
+        }
+        [Route("Create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                Plist.Add(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HandsOnAssignment/HandsOnAssignment/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HandsOnAssignment.Models
+{
+    public class Product
+    {   [Required(ErrorMessage ="Product Id Required")]
+        public string Pid { get; set; }
+        [Required(ErrorMessage = "Product Name Required")]
+        public string Pname { get; set; }
+        [Required(ErrorMessage = "Product Price Required")]
+        public int? Price { get; set; }
+        [Required(ErrorMessage = "Stock Required")]
+        public int? Stock { get; set; }
+
+    }
+}
